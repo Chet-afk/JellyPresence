92c8313f732a718f39ab8572b1a6dedd2826ebe1
Ken <kennethlieu@gmail.com>
2025-01-11 15:47:53 -0700

    Opens JMP lol

---
+++ JellyPresence.Project/Config.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace JellyPresence.Project
+{
+    /*
+     * Only holds functions for env related stuff
+     * until i come across things that might be needed lol
+     */
+
+    public class Config
+    {
+
+        /* 
+         * Ensure all needed fields are filled 
+         */
+        public static bool envIntegrity(IDictionary<string, string> _env)
+        {
+            return true;
+        }
+    }
+}
---
+++ JellyPresence.Project/JellyPresence.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using dotenv.net;
+
+namespace JellyPresence.Project
+{
+    public class JellyPresence
+    {
+
+        private readonly IDictionary<string, string> envDict;
+        private string jmpPath;
+
+        public JellyPresence()
+        {
+            envDict = DotEnv.Read();
+        }
+
+        public static void StartJMP() 
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "D:\\Jellyfin\\JellyfinMediaPlayer.exe";
+            process.Start();
+        }
+
+    }
+}
